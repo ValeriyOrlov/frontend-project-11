4a53d98d621a2b0229a5b5721f413067d4fb2cf4
Valerrriy Orlov <wrestlbird@gmail.com>
2023-04-17 23:24:30 +0300

    feat: add parseError msg

--- src/locales/en.js
+++ src/locales/en.js
@@ -2,6 +2,7 @@ export default {
     translation: {
         url: 'Link must be a valid URL',
         notOneOf: 'RSS is already exists',
+        parseError: 'The resource does not contain valid RSS',
         success: 'RSS uploaded successfully',
     }
 };
--- src/locales/ru.js
+++ src/locales/ru.js
@@ -2,6 +2,7 @@ export default {
     translation: {
         url: 'Ссылка должна быть валидным URL',
         notOneOf: 'RSS уже существует',
+        parseError: 'Ресурс не содержит валидный RSS',
         success: 'RSS успешно загружен',
     }
 };
