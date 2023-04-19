966f2290405e9ec18743b0496942ed0a7f76c919
Valerrriy Orlov <wrestlbird@gmail.com>
2023-04-19 23:08:34 +0300

    feat: implemented feed rendering

--- src/render.js
+++ src/render.js
@@ -1,3 +1,22 @@
+/*
+task: 
+
+*/
+
+const feedCardMaker = ({ title, description }) => {
+    const listGroupItem = document.createElement('li');
+    listGroupItem.classList.add('list-group-item', 'border-0', 'border-end-0');
+    const titleEl = document.createElement('h3');
+    titleEl.classList.add('h6', 'm-0');
+    titleEl.textContent = title;
+    const descriptionEl = document.createElement('p');
+    descriptionEl.classList.add('m-0', 'small', 'text-black-50');
+    descriptionEl.textContent = description;
+    listGroupItem.appendChild(titleEl);
+    listGroupItem.appendChild(descriptionEl);
+    return listGroupItem;
+} 
+
 const renderError = (fields, error) => {
     fields.rssInput.classList.add('is-invalid');
     if (!fields.rssInputFeedback.classList.contains('text-danger')) {
@@ -19,13 +38,24 @@ const renderSuccess = (elements, i18Instance) => {
     elements.fields.rssInputFeedback.textContent = i18Instance.t('success');
     elements.fields.rssInput.value = '';
     elements.fields.rssInput.focus();
-   /* const title = elements.data.title;
-    const description = elements.data.description;
-    const cardBorder = document.createElement('div');
-    cardBorder.classList('card, border-0');
-    //здесь дописываю обёртку оглавления фидов с шаблона и отрисовываю её
-    // нужно подумать, стоит ли выносить это в отдельную функцию
-    */
+    const cardBorder = document.createElement('div'); // начало отрисовки фидов
+    cardBorder.classList.add('card', 'border-0');
+    const cardBody = document.createElement('div');
+    const cardTitle = document.createElement('h2');
+    cardTitle.textContent = 'Фиды';
+    cardTitle.classList.add('card-title', 'h4');
+    const listGroup = document.createElement('ul');
+    listGroup.classList.add('list-group', 'border-0', 'rounded-0');
+    cardBorder.appendChild(cardBody);
+    cardBorder.appendChild(listGroup);
+    const newFeedCard = feedCardMaker(elements.data[elements.data.length - 1]);
+    if (document.querySelector('.card-title') === null) {
+        cardBody.appendChild(cardTitle);
+    }
+    listGroup.prepend(newFeedCard);
+    elements.feeds.appendChild(cardBorder)
+     // конец отрисовки фидов
+
 
 };
 
