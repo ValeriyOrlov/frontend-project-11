04d14c5aac5190c31286c15efdf373cf2074e9a3
Valerrriy Orlov <wrestlbird@gmail.com>
2023-05-06 18:58:16 +0300

    fix: lint errors, add: network err msg

--- src/locales/en.js
+++ src/locales/en.js
@@ -1,8 +1,9 @@
 export default {
-    translation: {
-        url: 'Link must be a valid URL',
-        notOneOf: 'RSS is already exists',
-        parseError: 'The resource does not contain valid RSS',
-        success: 'RSS uploaded successfully',
-    }
+  translation: {
+    url: 'Link must be a valid URL',
+    notOneOf: 'RSS is already exists',
+    parseError: 'The resource does not contain valid RSS',
+    success: 'RSS uploaded successfully',
+    AxiosError: 'Network Error',
+  },
 };
--- src/locales/ru.js
+++ src/locales/ru.js
@@ -1,8 +1,9 @@
 export default {
-    translation: {
-        url: 'Ссылка должна быть валидным URL',
-        notOneOf: 'RSS уже существует',
-        parseError: 'Ресурс не содержит валидный RSS',
-        success: 'RSS успешно загружен',
-    }
+  translation: {
+    url: 'Ссылка должна быть валидным URL',
+    notOneOf: 'RSS уже существует',
+    parseError: 'Ресурс не содержит валидный RSS',
+    success: 'RSS успешно загружен',
+    AxiosError: 'Ошибка сети',
+  },
 };
